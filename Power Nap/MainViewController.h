5a5b5507c04ac42e40ceae85349353d671b9ebd7
Paul Keller <Paul@Paul-Kellers-iMac.local>
2012-06-12 20:09:10 +0100

    Initial Commit

---
+++ Power Nap/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Power Nap
+//
+//  Created by Paul Keller on 12/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface MainViewController : UIViewController <FlipsideViewControllerDelegate>
+
+@end
